9139cc960bf5a89b2fff8df3ba7ea406b422cd85
baniasbaabe <banias@hotmail.de>
2022-01-06 18:02:18 +0100

    Added basic chitchat utters

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,41 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+import json
+from urllib.request import urlopen
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+
+
+class ActionGiveLocation(Action):
+
+    def name(self) -> Text:
+        return "action_give_location"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        try:
+            url = 'http://ipinfo.io/json'
+            response = urlopen(url)
+            data = json.load(response)
+
+            IP = data['ip']
+            org = data['org']
+            city = data['city']
+            country = data['country']
+            region = data['region']
+
+            message = f"I am living in {city}, {country}... Just like you ;)"
+        except:
+            message = "I am living in Stuttgart, DE."
+
+        dispatcher.utter_message(text = message)
+
+        return []
