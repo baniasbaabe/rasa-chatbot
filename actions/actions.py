a61a39b1ac8ebffd45f905422fecb1a7ab951ab1
baniasbaabe <banias@hotmail.de>
2022-01-25 20:55:13 +0100

    Add faq, add assets, add dataframe for dishes

--- actions/actions.py
+++ actions/actions.py
@@ -4,45 +4,16 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
-
 from typing import Any, Text, Dict, List
 
 import json
 import requests
 import datetime
 from urllib.request import urlopen
+from PIL import Image
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
-# class ActionGiveLocation(Action):
-
-#     def name(self) -> Text:
-#         return "action_give_location"
-
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-#         try:
-#             url = 'http://ipinfo.io/json'
-#             response = urlopen(url)
-#             data = json.load(response)
-
-#             IP = data['ip']
-#             org = data['org']
-#             city = data['city']
-#             country = data['country']
-#             region = data['region']
-
-#             message = f"I am living in {city}, {country}... Just like you ;)"
-#         except:
-#             message = "I am living in Stuttgart, DE."
-
-#         dispatcher.utter_message(text = message)
-
-#         return []
 
 class ActionGiveWeather(Action):
 
@@ -130,3 +101,31 @@ class ActionGiveDay(Action):
         dispatcher.utter_message(text = message)
 
         return []
+
+# class ActionGiveLocation(Action):
+
+#     def name(self) -> Text:
+#         return "action_give_location"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+#         try:
+#             url = 'http://ipinfo.io/json'
+#             response = urlopen(url)
+#             data = json.load(response)
+
+#             IP = data['ip']
+#             org = data['org']
+#             city = data['city']
+#             country = data['country']
+#             region = data['region']
+
+#             message = f"I am living in {city}, {country}... Just like you ;)"
+#         except:
+#             message = "I am living in Stuttgart, DE."
+
+#         dispatcher.utter_message(text = message)
+
+#         return []
